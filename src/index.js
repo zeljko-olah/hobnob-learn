ae1f9c7dcde291561e3435d1b9405c27f64a9a7e
Zeljko <olah.zeljko@gmail.com>
2019-10-03 08:01:33 +0200

    Handle malformed/non-JSON payloads for POST /user

--- src/index.js
+++ src/index.js
@@ -3,17 +3,54 @@ import http from 'http';
 
 function requestHandler(req, res) {
   if (req.method === 'POST' && req.url === '/users') {
-    res.writeHead(400, { 'Content-Type': 'application/json' });
-    res.end(
-      JSON.stringify({
-        message: 'Payload should not be empty',
-      }),
-    );
-    return;
+    const payloadData = [];
+    const PAYLOAD_LIMIT = 1e6;
+    req.on('data', (data) => {
+      payloadData.push(data);
+      const bodyString = Buffer.concat(payloadData).toString();
+      if (bodyString.length > PAYLOAD_LIMIT) {
+        res.writeHead(413, { 'Content-Type': 'text/plain' });
+        res.end();
+        res.connection.destroy();
+      }
+    });
+
+    req.on('end', () => {
+      if (payloadData.length === 0) {
+        res.writeHead(400, { 'Content-Type': 'application/json' });
+        res.end(
+          JSON.stringify({
+            message: 'Payload should not be empty',
+          }),
+        );
+        return;
+      }
+      if (req.headers['content-type'] !== 'application/json') {
+        res.writeHead(415, { 'Content-Type': 'application/json' });
+        res.end(
+          JSON.stringify({
+            message: 'The "Content-Type" header must always be "application/json"',
+          }),
+        );
+        return;
+      }
+      try {
+        const bodyString = Buffer.concat(payloadData).toString();
+        JSON.parse(bodyString);
+      } catch (e) {
+        res.writeHead(400, { 'Content-Type': 'application/json' });
+        res.end(
+          JSON.stringify({
+            message: 'Payload should be in JSON format',
+          }),
+        );
+      }
+    });
+  } else {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello, World!');
   }
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!');
 }
 
 const server = http.createServer(requestHandler);
-server.listen(8080); // Note that there is a newline below this line
+server.listen(8080);
