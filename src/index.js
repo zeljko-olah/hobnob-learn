ec3535c66132315a1296237314209d0523005a53
Zeljko <olah.zeljko@gmail.com>
2019-10-05 08:38:22 +0200

    Extract middlewares into modules

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import elasticsearch from 'elasticsearch';
 
+import checkEmptyPayload from './middlewares/check-empty-payload';
+import checkContentTypeIsSet from './middlewares/check-content-type-is-set';
+import checkContentTypeIsJson from './middlewares/check-content-type-is-json';
+import errorHandler from './middlewares/error-handler';
+
 const client = new elasticsearch.Client({
   host: `${process.env.ELASTICSEARCH_PROTOCOL}://${process.env.ELASTICSEARCH_HOSTNAME}:${process.env.ELASTICSEARCH_PORT}`
 });
@@ -10,59 +15,12 @@ const client = new elasticsearch.Client({
 const app = express();
 app.use(bodyParser.json({ limit: 1e6 }));
 
-function checkEmptyPayload(req, res, next) {
-  if (['POST', 'PATCH', 'PUT'].includes(req.method) && req.headers['content-length'] === '0') {
-    res.status(400);
-    res.set('Content-Type', 'application/json');
-    res.json({
-      message: 'Payload should not be empty'
-    });
-  }
-  next();
-}
-
-function checkContentTypeIsSet(req, res, next) {
-  if (
-    req.headers['content-length']
-    && req.headers['content-length'] !== '0'
-    && !req.headers['content-type']
-  ) {
-    res.status(400);
-    res.set('Content-Type', 'application/json');
-    res.json({
-      message: 'The "Content-Type" header must be set for requests with a non-empty payload'
-    });
-  }
-  next();
-}
-
-function checkContentTypeIsJson(req, res, next) {
-  if (!req.headers['content-type'].includes('application/json')) {
-    res.status(415);
-    res.set('Content-Type', 'application/json');
-    res.json({ message: 'The "Content-Type" header must always be "application/json"' });
-  }
-  next();
-}
-
 app.use(checkEmptyPayload);
 app.use(checkContentTypeIsSet);
 app.use(checkContentTypeIsJson);
 app.use(bodyParser.json({ limit: 1e6 }));
 
-app.use((err, req, res, next) => {
-  if (
-    err instanceof SyntaxError
-    && err.status === 400
-    && 'body' in err
-    && err.type === 'entity.parse.failed'
-  ) {
-    res.status(400);
-    res.set('Content-Type', 'application/json');
-    res.json({ message: 'Payload should be in JSON format' });
-  }
-  next();
-});
+app.use(errorHandler);
 
 app.post('/users', (req, res) => {
   if (
---
+++ src/middlewares/error-handler.js
@@ -0,0 +1,15 @@
+function errorHandler(err, req, res, next) {
+  if (
+    err instanceof SyntaxError
+    && err.status === 400
+    && 'body' in err
+    && err.type === 'entity.parse.failed'
+  ) {
+    res.status(400);
+    res.set('Content-Type', 'application/json');
+    res.json({ message: 'Payload should be in JSON format' });
+  }
+  next();
+}
+
+export default errorHandler;
